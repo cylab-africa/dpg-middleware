5b078cd4934b6d2cb76ef000503c3f74eb80065a
Jean Paul NISHIMIRWE <nishimirwepaul2015@gmail.com>
2022-07-25 17:25:40 +0200

    Changes

--- dhis2-mosip-middleware/pages/utils/aes.encrypt.js
+++ dhis2-mosip-middleware/pages/utils/aes.encrypt.js
@@ -6,10 +6,10 @@ export function cipheringText(text)
 {    
     // console.log(crypto.randomBytes(32).toString('hex')); // generate 32 bytes random string
     let cipher = crypto.createCipheriv(ALGORITHM, KEY, IV);  
+    
     let encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
     return encrypted;
 }
-
 
 export function decipheringText(encrypted)
 {    
