41dd48596c4c1a43a37e0c8b3534d954d8eced7b
Jean Paul NISHIMIRWE <nishimirwepaul2015@gmail.com>
2022-07-26 13:34:30 +0200

    Have reorganized the generate Url

--- dhis2-mosip-middleware/next.config.js
+++ dhis2-mosip-middleware/next.config.js
@@ -4,5 +4,15 @@ const nextConfig = {
   swcMinify: true,
 }
 
+module.exports = {
+  env: {
+    MOSIP_BASE_ROUTE:'https://mosipcmuafrica.me/idauthentication/v1/',
+    API_ROUTE: 'http://front-end/',
+
+    ALGORITHM: 'aes256',
+    KEY: '791bd20f047032b27ba999425a9a8e61',
+    IV: '239fb9856dd1c9f3'
+  },
+}
 
 module.exports = nextConfig
--- dhis2-mosip-middleware/pages/api/url/keys.js
+++ dhis2-mosip-middleware/pages/api/url/keys.js
@@ -21,7 +21,7 @@ export default async function authenticate(req, res)
         const keys = `${req.body.misp_lk}|${req.body.auth_partner_id}|${req.body.api_key}|${req.body.transaction_id}|${req.body.callback_url}`;
         const encrypted  = cipheringText(keys);
 
-        const url = `${API_ROUTE}${encrypted}`
+        const url = `${process.env.API_ROUTE}${encrypted}`
         console.log(encrypted, decipheringText(encrypted));
 
         return res.status(200).json({success: true, url})
